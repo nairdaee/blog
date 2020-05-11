b519c67c8be2a03b0f56605cf464aeb8057d2b26
Adrian Etenyi <etadriano2@gmail.com>
2020-05-11 21:35:22 +0300

    add users class

--- app/models.py
+++ app/models.py
@@ -46,3 +46,33 @@ class Comment(db.Model):
     def get_comments(cls,id):
         comments = Comments.query.filter_by(blog_id=id).all()
         return comments
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+      #upvotes = db.relationship('Upvote', backref='user', lazy='dynamic')
+      #downvotes = db.relationship('Downvote', backref='user', lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
