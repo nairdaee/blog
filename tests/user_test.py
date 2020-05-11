5affe0df2c486c906ad0dc8ccc07c9a5892fc7f1
Adrian Etenyi <etadriano2@gmail.com>
2020-05-11 22:27:40 +0300

    add test for user

---
+++ tests/user_test.py
@@ -0,0 +1,21 @@
+import unittest
+from app import db
+from app.models import User
+
+class UserModelTest(unittest.TestCase):
+
+    def setUp(self):
+        self.new_user = User(password='anything')
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('anything'))
+
+        import unittest
+
