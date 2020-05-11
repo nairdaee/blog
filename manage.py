33b14f3a597fd4941ab1593a48f7a9582961b128
Adrian Etenyi <etadriano2@gmail.com>
2020-05-11 21:23:43 +0300

    make correct import configurations

---
+++ manage.py
@@ -0,0 +1,16 @@
+from app import create_app,db
+from app.models import Blog
+from flask_script import Manager,Server
+from flask_migrate import Migrate,MigrateCommand
+
+app = create_app('development')
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Comment = Comment, Pitch = Pitch)
+if __name__ == '__main__':
+    manager.run()
